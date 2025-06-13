98398196ca8a69bd6951f0e5e0fc99a0ec46217f
Otavio <estudosprogotavio@gmail.com>
2025-06-13 13:30:12 -0300

    feat:adicionando entidade Importacao

---
+++ src/main/java/dev/Zerphyis/upload/domain/importacao/Importacao.java
@@ -0,0 +1,47 @@
+package dev.Zerphyis.upload.domain.importacao;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+@Entity
+@Table(name = "tb_importacao")
+public class Importacao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    private LocalDateTime dataHoraImportacao;
+    @NotNull
+    private LocalDate dataTransacao;
+
+    public Importacao() {
+    }
+
+    public LocalDate getDataTransacao() {
+        return dataTransacao;
+    }
+
+    public void setDataTransacao(LocalDate dataTransacao) {
+        this.dataTransacao = dataTransacao;
+    }
+
+    public LocalDateTime getDataHoraImportacao() {
+        return dataHoraImportacao;
+    }
+
+    public void setDataHoraImportacao(LocalDateTime dataHoraImportacao) {
+        this.dataHoraImportacao = dataHoraImportacao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
+
+
