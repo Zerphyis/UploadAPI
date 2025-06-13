220c8e9242ceb3fa44ded6cf886ae9461e982272
Otavio <estudosprogotavio@gmail.com>
2025-06-13 13:31:44 -0300

    feat:adicionando Controller de Transacao

---
+++ src/main/java/dev/Zerphyis/upload/infra/ControllerTransacao.java
@@ -0,0 +1,54 @@
+package dev.Zerphyis.upload.infra;
+
+import dev.Zerphyis.upload.aplication.TransacaoService;
+import dev.Zerphyis.upload.domain.importacao.Importacao;
+import dev.Zerphyis.upload.domain.transacoes.Transacao;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+@Controller
+@RequestMapping("/transacoes")
+public class ControllerTransacao {
+
+
+        private final TransacaoService transacaoService;
+
+        public ControllerTransacao(final TransacaoService transacaoService) {
+            this.transacaoService = transacaoService;
+        }
+
+        @GetMapping("/mostrar")
+        public String mostrarPaginaImportacao(Model model) {
+            List<Importacao> importacoes = transacaoService.listarTodasImportacoes();
+            model.addAttribute("importacoes", importacoes);
+            return "index";
+        }
+
+        @PostMapping("/importar")
+        public String importarTransacoes(@RequestParam("file") MultipartFile file, Model model) {
+            try {
+                transacaoService.processarEImportarTransacoes(file);
+                model.addAttribute("message", "Transações importadas com sucesso.");
+                List<Importacao> importacoes = transacaoService.listarTodasImportacoes();
+                model.addAttribute("importacoes", importacoes);
+                return "index";
+            } catch (Exception e) {
+                model.addAttribute("message", "Erro ao importar transações: " + e.getMessage());
+                return "transacao/resposta";
+            }
+        }
+
+        @GetMapping("/listar")
+        public String listarTransacoes(Model model) {
+            List<Transacao> transacoes = transacaoService.listarTodasTransacoes();
+            model.addAttribute("transacoes", transacoes);
+            return "transacao/listar";
+        }
+    }
+
